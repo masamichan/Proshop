856ec5a87770b3d670c1eec450f0409843725a73
masamichan <positivemasa031@gmail.com>
2023-11-22 22:45:15 +0900

    import Button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import { Row, Col, Image, ListGroup, Card } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import Rating from "../components/Rating";
 import products from "../products";
 
-function ProductScreen({ match }) {
+function ProductScreen() {
   const { id } = useParams();
   const product = products.find((p) => p._id === id);
   return (
     <div>
-      <Link to="/" className="btn btn-light my-3">
-        GO Back
+      <Link to="/">
+        <Button variant="dark">Go Back</Button>
         <Row>
           <Col md={6}>
             <Image src={product.image} alt={product.name} fluid />
