077ecfa99bdd636181aec45d773fe54fd980f330
masamichan <positivemasa031@gmail.com>
2023-11-29 15:05:13 +0900

    in the middle of serializing data

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Product
 from .products import products
+from .serializers import ProductSerializer
+
 
 # Create your views here.
 
@@ -27,5 +31,19 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
-def getProducts(request):
-  return Response(products)
+def getProduct(request, pk):
+    product = Product.objects.all()
+    serializer = ProductSerializer(products, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProducts(request,pk):
+  product = None
+  for i in products:
+    if i['_id'] == pk:
+      product = i
+      break
+
+    return Response(product)
+  
