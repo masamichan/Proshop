b96aac49b73bd7ce0d7f8f4148a3f7c2376cef7c
masamichan <positivemasa031@gmail.com>
2023-12-05 13:45:30 +0900

    数量選択＆カートに追加

--- frontend/src/screens/cartScreen.js
+++ frontend/src/screens/cartScreen.js
@@ -10,13 +10,16 @@ import {
   Button,
   Card,
 } from "react-bootstrap";
-import { Message } from "../components/Message";
+import Message from "../components/Message";
 import { addToCard } from "../actions/cartActions";
 
 function cartScreen({ match, location, history }) {
   const productId = match.params.id;
   const qty = location.search ? Number(location.search.split("=")[1]) : 1;
   const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { catItem } = cart;
 
   useEffect(() => {
     if (productId) {
@@ -26,7 +29,36 @@ function cartScreen({ match, location, history }) {
 
   return (
     <div>
-      <Cart></Cart>
+      <Cart>
+        <Row>
+          <Col md={8}>
+            <h1>shopping Cart</h1>
+            {cartItems.length === 0 ? (
+              <Message variant="info">
+                Your cart is empty <Link to="/" Go back></Link>
+              </Message>
+            ) : (
+              <ListGroup variant="flush">
+                {cartItems.map((item) => (
+                  <ListGroup key={item.product}>
+                    <Row>
+                      <Col md={2}>
+                        <Image src={item.image} alt={item.name} fluid rounded />
+                      </Col>
+                      <Col md={3}>
+                        <Link to={`/product/${product.id}`}> {item.name}</Link>
+                      </Col>
+                      <Col md={2}>${item.price}</Col>
+                    </Row>
+                    <Col md={3}></Col>
+                  </ListGroup>
+                ))}
+              </ListGroup>
+            )}
+          </Col>
+          <Col md={4}></Col>
+        </Row>
+      </Cart>
     </div>
   );
 }
